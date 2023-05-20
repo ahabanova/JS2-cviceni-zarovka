e7c603db2b7e3accc33463ed20335f1546a880a1
ahabanova <ahabanova95@gmail.com>
2023-05-20 17:43:00 +0200

    hotovo

---
+++ Bulb/index.js
@@ -0,0 +1,17 @@
+export const Bulb = (props) => {
+    const bulbElm = document.createElement('div')
+    bulbElm.classList.add('bulb')
+
+    if (props.isOn) {
+        bulbElm.classList.add('bulb--on')
+    }
+
+    bulbElm.addEventListener('click', () => {
+        bulbElm.replaceWith(Bulb({
+            isOn: !props.isOn,
+        }
+        ))
+    })
+
+    return bulbElm
+}
--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('funguju!');
+import { Bulb } from "./Bulb/index.js";
+
+const bulb1 = {
+    isOn: true,
+}
+
+const bulb2 = {
+    isOn: false,
+}
+
+const bulb3 = {
+    isOn: false,
+}
+
+
+const appElm = document.querySelector('#app')
+appElm.append(Bulb(bulb1), Bulb(bulb2), Bulb(bulb3))
